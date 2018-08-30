01d56838a6dcf9962b8cad61ba2e8f27e6fe6607
wulei <lei.wu@htouhui.com>
2018-08-30 13:07:16 +0800

    添加html-webpack-plugin插件，以及修改相应配置

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports={
     //入口文件的配置项
     entry:{
@@ -15,12 +17,18 @@ module.exports={
     //模块：例如解读CSS,图片如何转换，压缩
     module:{},
     //插件，用于生产模版和各项功能
-    plugins:[],
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ],
     //配置webpack开发服务功能
     devServer:{
         historyApiFallback: true,//不跳转
         inline: true,
-        hot: true,//实时刷新
         host:'192.168.1.79',
         compress:true,
         port:9000
